c349f58ef6eb59248f7cc6b803a1013c21e29b92
benjamin <benjamin.olmsted@gmail.com>
2021-08-10 10:09:20 -0700

    commit tree 33709dc637c3e595c22c9e817acd48b3426ba003

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(canidates)
+        canidates.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
